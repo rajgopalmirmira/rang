66528283fa04e4437908ae40aa70ef7ae7eb1da7
rajgopalmirmira <raj.gopal12397@gmail.com>
2016-02-11 16:33:32 -0500

    One last time

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Rang
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.initialize_on_percompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
