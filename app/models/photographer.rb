f1ef85252f416f261504e7f3694f0cb89d3c049e
rajgopalmirmira <raj.gopal12397@gmail.com>
2016-02-13 19:47:28 -0500

    trying one

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,5 +1,5 @@
 class Photographer < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image , :style =>  { :medium => "400x400>" , :thumb => "100x100>"}
+  has_attached_file :image , :styles =>  { :medium => "300x300>" , :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
