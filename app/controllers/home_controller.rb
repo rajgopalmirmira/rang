aaa25e048384010176721ad0d644450959991719
rajgopalmirmira <raj.gopal12397@gmail.com>
2016-02-16 12:27:16 -0500

    homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in? then
-      @login_user_name = current_user.email;
-    else
-      @login_user_name="Login";
-    end
+
   end
 
   def about
