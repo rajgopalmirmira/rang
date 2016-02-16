c7949097354029d1c4c8ccd331210fa113cbb6b4
rajgopalmirmira <raj.gopal12397@gmail.com>
2016-02-16 12:06:10 -0500

    with home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
-//=require bootstrap-sprockets
 //=require bootstrap
 //= require masonry/jquery.masonry
 //= require_tree .
